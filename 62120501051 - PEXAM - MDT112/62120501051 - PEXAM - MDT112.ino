eaa43bca256ef13e067787c798b7e389b31ffddf
Penguin SAMA <kingdarkkurumi@hotmail.com>
2020-07-04 14:42:47 +0700

    5.complete

--- 62120501051 - PEXAM - MDT112/62120501051 - PEXAM - MDT112.ino
+++ 62120501051 - PEXAM - MDT112/62120501051 - PEXAM - MDT112.ino
@@ -51,16 +51,58 @@
   //delay(2000);
 //}
 
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-void setup()
-{
-  lcd.begin();
-  lcd.backlight();
-  lcd.setCursor(2, 1); 
-  lcd.print("Hello MDT"); 
+//#include <Wire.h>
+//#include <LiquidCrystal_I2C.h>
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
+//void setup()
+//{
+  //lcd.begin();
+  //lcd.backlight();
+  //lcd.setCursor(2, 1); 
+  //lcd.print("Hello MDT"); 
+//}
+//void loop() 
+//{
+  //lcd.setCursor(0, 0);
+  //lcd.print(" Hello MDT");
+  //lcd.setCursor(0, 1);
+  //lcd.print(" void loop(){ ");
+  //lcd.clear(); // ล้างหน้าจอ
+//}
+//#include <Wire.h>
+//#include <LiquidCrystal_I2C.h>
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
+//int MotorPin2 = 2;
+//int MotorPin3 = 3;
+//void setup()
+//{
+    //pinMode(MotorPin2, OUTPUT);
+    //pinMode(MotorPin3, OUTPUT);
+    //Serial.begin(9600);
+    //lcd.begin();
+    //lcd.backlight();
+    //lcd.setCursor(2, 1); 
+    //lcd.print("Turn CCW"); 
+    //delay(3000);
+//}
+//void loop()
+//{
+  //digitalWrite(MotorPin2, HIGH); 
+  //digitalWrite(MotorPin3, LOW); 
+  //Serial.println("Motor HIGHT");
+  //delay(3000);
+//}
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+
+void setup() {
+  lcd.begin(16, 2); // จอกว้าง 16 ตัวอักษร 2 บรรทัด
+  lcd.print(" Hello MDT "); 
+  lcd.setCursor(0, 1); // เลื่อนเคเซอร์ไปบรรทัดที่ 2 ลำดับที่ 0 (ก่อนหน้าตัวอักษรแรก)
+  delay(1000); // หน่วงเวลา 1 วินาที
+  lcd.clear(); // ล้างหน้าจอ
 }
+
 void loop() 
 {
   lcd.setCursor(0, 0);
@@ -68,27 +110,4 @@ void loop()
   lcd.setCursor(0, 1);
   lcd.print(" void loop(){ ");
   lcd.clear(); // ล้างหน้าจอ
-}
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-int MotorPin2 = 2;
-int MotorPin3 = 3;
-void setup()
-{
-    pinMode(MotorPin2, OUTPUT);
-    pinMode(MotorPin3, OUTPUT);
-    Serial.begin(9600);
-    lcd.begin();
-    lcd.backlight();
-    lcd.setCursor(2, 1); 
-    lcd.print("Turn CCW"); 
-    delay(3000);
-}
-void loop()
-{
-  digitalWrite(MotorPin2, HIGH); 
-  digitalWrite(MotorPin3, LOW); 
-  Serial.println("Motor HIGHT");
-  delay(3000);
 }
