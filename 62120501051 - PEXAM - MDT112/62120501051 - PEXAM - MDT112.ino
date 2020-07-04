c9934413b6806c84d06f89c275187da78ab73bbf
Penguin SAMA <kingdarkkurumi@hotmail.com>
2020-07-04 14:30:52 +0700

    2.complete

--- 62120501051 - PEXAM - MDT112/62120501051 - PEXAM - MDT112.ino
+++ 62120501051 - PEXAM - MDT112/62120501051 - PEXAM - MDT112.ino
@@ -1,9 +1,28 @@
-const int buzzer = 9;
-void setup() {
-    pinMode(buzzer, OUTPUT);
+//const int buzzer = 9;
+//void setup() {
+//pinMode(buzzer, OUTPUT);
+//void loop() 
+//{
+//tone(buzzer, 800); 
+//delay(400);        
+//noTone(buzzer);     
+ //}
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup()
+{
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(2, 1); 
+  lcd.print("Hello MDT"); 
+}
 void loop() 
 {
-  tone(buzzer, 800); 
-  delay(400);        
-  noTone(buzzer);     
- }
+  lcd.setCursor(0, 0);
+  lcd.print(" Hello MDT");
+  lcd.setCursor(0, 1);
+  lcd.print(" void loop(){ ");
+  lcd.clear(); // ล้างหน้าจอ
+}
