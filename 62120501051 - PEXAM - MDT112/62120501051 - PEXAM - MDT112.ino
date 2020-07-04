0ec39dcdf1979622a8c0b6b6cc82935cea8d56ab
Penguin SAMA <kingdarkkurumi@hotmail.com>
2020-07-04 14:36:24 +0700

    3.complete

--- 62120501051 - PEXAM - MDT112/62120501051 - PEXAM - MDT112.ino
+++ 62120501051 - PEXAM - MDT112/62120501051 - PEXAM - MDT112.ino
@@ -8,21 +8,45 @@
 //noTone(buzzer);     
  //}
 
+//#include <Wire.h>
+//#include <LiquidCrystal_I2C.h>
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
+//void setup()
+//{
+  //lcd.begin();
+  //lcd.backlight();
+  //lcd.setCursor(2, 1); 
+  //lcd.print("Hello MDT"); 
+//}
+//void loop() 
+//{
+  //lcd.setCursor(0, 0);
+  //lcd.print(" Hello MDT");
+  //lcd.setCursor(0, 1);
+  //lcd.print(" void loop(){ ");
+  //lcd.clear(); // ล้างหน้าจอ
+//}
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+int MotorPin2 = 2;
+int MotorPin3 = 3;
 void setup()
 {
-  lcd.begin();
-  lcd.backlight();
-  lcd.setCursor(2, 1); 
-  lcd.print("Hello MDT"); 
+    pinMode(MotorPin2, OUTPUT);
+    pinMode(MotorPin3, OUTPUT);
+    Serial.begin(9600);
+    lcd.begin();
+    lcd.backlight();
+    lcd.setCursor(2, 1); 
+    lcd.print("Turn CCW"); 
+    delay(2000);
 }
-void loop() 
+void loop()
 {
-  lcd.setCursor(0, 0);
-  lcd.print(" Hello MDT");
-  lcd.setCursor(0, 1);
-  lcd.print(" void loop(){ ");
-  lcd.clear(); // ล้างหน้าจอ
+  digitalWrite(MotorPin2, LOW); 
+  digitalWrite(MotorPin3, HIGH); 
+  Serial.println("Motor left");
+  delay(2000);
 }
